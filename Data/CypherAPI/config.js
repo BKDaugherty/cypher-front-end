ea5c8a17f73d2b21674150d4de78d0f4831d9479
Brendon Daugherty <brendon1097@gmail.com>
2018-04-11 21:13:25 -0700

    Point CypherApp at API

--- Data/CypherAPI/config.js
+++ Data/CypherAPI/config.js
@@ -1,9 +1,8 @@
 
 // export const BASEURL = "http://192.168.0.109:8000"
 // export const BASEURL = "http://localhost:8000"
-export const BASEURL = "http://192.168.0.22:8000"
-//export const BASEURL = "https://cypher.herokuapp.com"
-//1182fd792318895dc13240834b4e57e5fd391380d3a2295c18a99869ebb001bf
+// export const BASEURL = "http://192.168.0.22:8000"
+export const BASEURL = "https://cypher.herokuapp.com"
 export const Endpoints = {
     userSignUp:'/user/sign-up',
     userLogin:'/user/login',
